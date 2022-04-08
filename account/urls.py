acd0800dbca8d650b601a77fe05afae665bae29b
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-08 22:40:12 +0100

    reset password views

--- account/urls.py
+++ account/urls.py
@@ -4,13 +4,30 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    # default view
     # path('login/', views.user_login, name='login'),
+    
+    # django auth auth_views
+    # login & logout
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('', views.dashboard, name='dashboard'),
-
+    # password change
     path('password-change/', auth_views.PasswordChangeView.as_view(),
          name='password_change'),
     path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(),
          name='password_change_done'),
+    # password reset
+    path('password-reset/',
+         auth_views.PasswordResetView.as_view(),
+         name='password_reset'),
+    path('password-reset/done/',
+         auth_views.PasswordResetDoneView.as_view(),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',
+         auth_views.PasswordResetConfirmView.as_view(),
+         name='password_reset_confirm'),
+    path('reset/done/',
+         auth_views.PasswordResetCompleteView.as_view(),
+         name='password_reset_complete'),
 ]
--- socialwebsite/settings.py
+++ socialwebsite/settings.py
@@ -124,6 +124,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+# emails
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
